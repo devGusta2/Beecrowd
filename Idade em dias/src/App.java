e539ec03a73b5fdf5f1a09587bae2c67f2d5db89
devGusta2 <gustavo.souza2979@gmail.com>
2024-05-11 15:12:17 -0300

    Idade em dias

--- Idade em dias/src/App.java
+++ Idade em dias/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
+        int dia, mes, ano;
+        dia=in.nextInt();
         
+  
+        mes=dia/30;
+        ano=mes/12;
+        mes=mes%12;
+        dia=dia%mes;
+        
+        System.out.println("Anos:"+ano);
+        System.out.println("Mes:"+mes);
+        System.out.println("Dias:"+dia);
     }
 }
